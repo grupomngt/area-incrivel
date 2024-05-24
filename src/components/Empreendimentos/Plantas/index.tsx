d3cb9f57a3979f25600b1ad2b0dcab6cd8d28dc9
grupomngt <166436490+grupomngt@users.noreply.github.com>
2024-05-24 10:45:16 -0300

    Update index.tsx

--- src/components/Empreendimentos/Plantas/index.tsx
+++ src/components/Empreendimentos/Plantas/index.tsx
@@ -21,8 +21,8 @@ export function Plantas() {
             }
             preco="R$1.047,00"
             linkButton="https://api.whatsapp.com/send?phone=551930970232&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20as%20casas%20de%2050m%C2%B2"
-            img="https://res.cloudinary.com/duv6mjghr/image/upload/v1712086280/casade52_lpf4hn.png"
-            imgModal="https://res.cloudinary.com/duv6mjghr/image/upload/v1712086181/casa52_nmfozf.png"
+            img="https://primeiro.rioclarofc.com.br/~grupomn/casa52.png"
+            imgModal="https://primeiro.rioclarofc.com.br/~grupomn/casa52.png"
           />
           <CardEmpreendimento
             title={
@@ -32,14 +32,14 @@ export function Plantas() {
             }
             preco="R$1.238,00"
             linkButton="https://api.whatsapp.com/send?phone=551930970232&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20as%20casas%20de%2060m%C2%B2"
-            img="https://res.cloudinary.com/duv6mjghr/image/upload/v1712086279/casade60_sa1n9t.png"
-            imgModal="https://res.cloudinary.com/duv6mjghr/image/upload/v1712086182/casa60_yy2kyl.png"
+            img="https://primeiro.rioclarofc.com.br/~grupomn/casa60.png"
+            imgModal="https://primeiro.rioclarofc.com.br/~grupomn/casa60.png"
           />
         </ContentCards>
       </Content>
       <Image>
         <img
-          src="https://res.cloudinary.com/duv6mjghr/image/upload/v1711492202/public/family2_wgvr3b.png"
+          src="https://primeiro.rioclarofc.com.br/~grupomn/family2.png"
           alt=""
         />
       </Image>
