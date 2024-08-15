1732e5ae7d90f0a5ac33e088456976439e611a60
grupomngt <166436490+grupomngt@users.noreply.github.com>
2024-08-15 13:41:59 -0300

    Update index.tsx

--- src/components/Empreendimentos/Plantas/index.tsx
+++ src/components/Empreendimentos/Plantas/index.tsx
@@ -7,42 +7,6 @@ import { List, ListItem } from "../../elements/CardEmpreendimento/styles";
 
 export function Plantas() {
   return (
-    <Container>
-      <Content>
-        <h1>
-          Casas in<span>críveis</span>
-        </h1>
-        <ContentCards>
-          <CardEmpreendimento
-            title={
-              <h2>
-                CASAS DE 52<span>m²</span>
-              </h2>
-            }
-            preco="R$1.047,00"
-            linkButton="https://api.whatsapp.com/send?phone=551930970232&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20as%20casas%20de%2050m%C2%B2"
-            img="https://primeiro.rioclarofc.com.br/~grupomn/casa52.png"
-            imgModal="https://primeiro.rioclarofc.com.br/~grupomn/casa52.png"
-          />
-          <CardEmpreendimento
-            title={
-              <h2>
-                CASAS DE 60<span>m²</span>
-              </h2>
-            }
-            preco="R$1.238,00"
-            linkButton="https://api.whatsapp.com/send?phone=551930970232&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20as%20casas%20de%2060m%C2%B2"
-            img="https://primeiro.rioclarofc.com.br/~grupomn/casa60.png"
-            imgModal="https://primeiro.rioclarofc.com.br/~grupomn/casa60.png"
-          />
-        </ContentCards>
-      </Content>
-      <Image>
-        <img
-          src="https://primeiro.rioclarofc.com.br/~grupomn/family2.png"
-          alt=""
-        />
-      </Image>
-    </Container>
+    <span></span>
   );
 }
